00b004fd290abcbc1e1e1858ec316d057adea62e
Jino <krt8599@naver.com>
2026-05-08 19:34:27 +0900

    feat: initialize ringdu server health check

--- src/test/java/com/ringdu/ringduserver/RingduServerApplicationTests.java
+++ src/test/java/com/ringdu/ringduserver/RingduServerApplicationTests.java
@@ -2,7 +2,9 @@ package com.ringdu.ringduserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class RingduServerApplicationTests {
 
