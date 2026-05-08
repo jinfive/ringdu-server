764f9370d2a112f31526a2fe62371d17a7f5ba02
Jino <krt8599@naver.com>
2026-05-08 19:09:25 +0900

    init ringdu server project

---
+++ src/main/java/com/ringdu/ringduserver/RingduServerApplication.java
@@ -0,0 +1,13 @@
+package com.ringdu.ringduserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingduServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RingduServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ringdu/ringduserver/RingduServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ringdu.ringduserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RingduServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
